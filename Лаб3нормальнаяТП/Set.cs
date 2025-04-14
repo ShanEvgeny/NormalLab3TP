169d3d7c9223b77f60e564f8f348aec3ab0c3031
ShanEvgeny <saevgenij76@gmail.com>
2025-04-14 21:41:43 +0800

    Добавьте файлы проекта.

---
+++ Лаб3нормальнаяТП/Set.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб3нормальнаяТП
+{
+    public class Set
+    {
+        private List<int> values = new List<int>();
+        public Set(List<int> values)
+        {
+            this.values = values;
+        }
+        public List<int> GetValues()
+        {
+            return values;
+        }
+        public override string ToString()
+        {
+            return String.Join(" ", this.values);
+        }
+        public static Set operator +(Set set, int number)
+        {
+            if (!set.values.Contains(number))
+                set.values.Add(number);
+            var newSet = new Set(set.values);
+            return newSet;
+        }
+        public static Set operator +(int number, Set set)
+        {
+            return set + number;
+        }
+        public static Set operator +(Set set1, Set set2)
+        {
+            var newSet = new Set(new List<int>());
+            foreach(var number in set1.values)
+            {
+                if (!newSet.values.Contains(number))
+                    newSet.values.Add(number);
+            }
+            foreach (var number in set2.values)
+            {
+                if (!newSet.values.Contains(number)) 
+                    newSet.values.Add(number);
+            }
+            return newSet;
+        }
+        public static Set operator -(Set set, int number)
+        {
+            if (set.values.Contains(number))
+                set.values.Remove(number);
+            var newSet = new Set(set.values);
+            return newSet;
+        }
+        public static Set operator *(Set set1, Set set2)
+        {
+            var newSet = new Set(new List<int>());
+            foreach (var number in set1.values)
+            {
+                if (set1.values.Contains(number) && set2.values.Contains(number))
+                    newSet.values.Add(number);
+            }
+            foreach (var number in set2.values)
+            {
+                if (set1.values.Contains(number) && set2.values.Contains(number) && !newSet.values.Contains(number))
+                    newSet.values.Add(number);
+            }
+            return newSet;
+        }
+        public static Set operator -(Set set1, Set set2)
+        {
+            var newSet = new Set(new List<int>());
+           foreach(var number in set1.values)
+            {
+                if (!set2.values.Contains(number))
+                    newSet.values.Add(number);
+            }
+            return newSet;
+        }
+    }
+}
---
+++ Лаб3нормальнаяТПTests/SetTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Лаб3нормальнаяТП;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лаб3нормальнаяТП.Tests
+{
+    [TestClass()]
+    public class SetTests
+    {
+        [TestMethod()]
+        public void PrintTest()
+        {
+            var set = new Set(new List<int> { 1, 2, 3, 4, -5, 0 });
+            Assert.AreEqual("1 2 3 4 -5 0", set.ToString());
+        }
+        [TestMethod()]
+        public void AddNumberTest()
+        {
+            var set = new Set(new List<int> { 1, 2, 3, 4, -5, 0 });
+            set = set + 8;
+            Assert.AreEqual("1 2 3 4 -5 0 8", set.ToString());
+        }
+        [TestMethod()]
+        public void SumSetsTest()
+        {
+            var set1 = new Set(new List<int> { 1, 2, 3, 4, 6 });
+            var set2 = new Set(new List<int> { 2, 4, 5, 7 });
+            var set = set1 + set2;
+            Assert.AreEqual("1 2 3 4 6 5 7", set.ToString());
+        }
+        [TestMethod()]
+        public void DeleteNumberTest()
+        {
+            var set = new Set(new List<int> { 1, 2, 3, 4, 6 });
+            set = set - 3;
+            Assert.AreEqual("1 2 4 6", set.ToString());
+        }
+        [TestMethod()]
+        public void DifferenceSetsTest()
+        {
+            var set1 = new Set(new List<int> { 1, 2, 3, 4, 6 });
+            var set2 = new Set(new List<int> { 2, 4, 5, 7 });
+            var set = set1 - set2;
+            Assert.AreEqual("1 3 6", set.ToString());
+        }
+
+        [TestMethod()]
+        public void MultiplicationSetsTest()
+        {
+            var set1 = new Set(new List<int> { 1, 2, 3, 4, 6 });
+            var set2 = new Set(new List<int> { 2, 4, 5, 7 });
+            var set = set1 * set2;
+            Assert.AreEqual("2 4", set.ToString());
+        }
+    }
+}
